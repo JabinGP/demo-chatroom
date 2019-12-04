565a423a4649d207f0b30e075771da941826b2b1
JabinGP <1748511181@qq.com>
2019-12-04 10:15:40 +0800

    feat: get message time limited

--- controllers/message.go
+++ controllers/message.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"time"
+
 	"github.com/JabinGP/demo-chatroom/models"
 	"github.com/iris-contrib/middleware/jwt"
 	"github.com/kataras/iris/v12"
@@ -22,6 +24,7 @@ func SendMessage(ctx iris.Context) {
 		SenderID:   userID,
 		ReceiverID: req.ReceiverID,
 		Content:    req.Content,
+		SendTime:   time.Now(),
 	}
 
 	if err := db.Table("messages").Create(&newMsg).Error; err != nil {
@@ -34,24 +37,49 @@ func SendMessage(ctx iris.Context) {
 
 // GetMessageList get message that send to me
 func GetMessageList(ctx iris.Context) {
+	type Req struct {
+		Time      int64 `json:"time"`
+		BeginTime int64 `json:"beginTime"`
+		EndTime   int64 `json:"endTime"`
+	}
 	type Res struct {
+		ID         uint   `json:"id"`
 		SenderID   uint   `json:"senderId"`
 		SenderName string `json:"senderName" gorm:"column:username"`
 		Content    string `json:"content"`
+		SendTime   int64  `json:"sendTime"`
 	}
 
+	req := Req{}
+	ctx.ReadQuery(&req)
 	jwtInfo := ctx.Values().Get("jwt").(*jwt.Token).Claims.(jwt.MapClaims)
 	userID := uint(jwtInfo["userId"].(float64))
 
 	msgList := []Res{}
-	if err := db.Table("messages").
-		Select("*,users.username").
-		Where("receiver_id = ?", userID).
+
+	tmpDB := db.Table("messages").
 		Joins("left join users on messages.sender_id = users.id").
-		Find(&msgList).Error; err != nil {
-		ctx.JSON(new(models.ResModel).WithError(err.Error()))
-		return
+		Select(`messages.id,
+						unix_timestamp(messages.send_time) as send_time, 
+						users.username,
+						messages.sender_id,
+						messages.content`)
+
+	if req.EndTime == 0 { // no limited end time
+		tmpDB = tmpDB.Where(`(messages.receiver_id = ? 
+													or messages.receiver_id = 0)
+													and unix_timestamp(messages.send_time) >= ? `,
+			userID, req.BeginTime)
+	} else { // has limited end time
+		tmpDB = tmpDB.Where(`(messages.receiver_id = ? 
+													or messages.receiver_id = 0)
+													and unix_timestamp(messages.send_time) >= ? 
+													and unix_timestamp(messages.send_time) <= ? `,
+			userID, req.BeginTime, req.EndTime)
 	}
 
+	if err := tmpDB.Find(&msgList).Error; err != nil {
+		ctx.JSON(new(models.ResModel).WithError(err.Error()))
+	}
 	ctx.JSON(new(models.ResModel).WithData(msgList))
 }
--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/jinzhu/gorm v1.9.11
 	github.com/kataras/iris v11.1.1+incompatible
 	github.com/kataras/iris/v12 v12.0.1
+	github.com/mdempsky/gocode v0.0.0-20191202075140-939b4a677f2f // indirect
+	github.com/nsf/gocode v0.0.0-20190302080247-5bee97b48836 // indirect
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -18,5 +20,7 @@ require (
 	github.com/spf13/viper v1.5.0
 	golang.org/x/sys v0.0.0-20191128015809-6d18c012aee9 // indirect
 	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20191204011308-9611592c72f6 // indirect
+	golang.org/x/tools/gopls v0.2.1 // indirect
 	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
